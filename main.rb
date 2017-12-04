13bf02e8d235906e37d5b1e09887f8511804380e
Ori Pekelman <ori@platform.sh>
2017-12-04 13:43:32 +0100

    influxitude

--- main.rb
+++ main.rb
@@ -39,9 +39,20 @@ class Main < Sinatra::Base
       session.commit
       message+= "Solr successful"
 
-      name     = 'foobar'      
-      influxdb = InfluxDB::Client.new url: ENV['INFLUXDB_URL']
+      name     = 'foobar'
+      database = 'baz'
 
+      username = 'foobar'
+      password = 'pwd'
+
+
+      influxdb = InfluxDB::Client.new url: ENV['INFLUXDB_URL']
+      influxdb.create_cluster_admin(username, password)
+      influxdb.config.username = username
+      influxdb.config.password = password
+      influxdb.create_database(database)
+      influxdb.config.database = database
+      
       # Enumerator that emits a sine wave
       Value = (0..10).to_a.map {|i| Math.send(:sin, i / 10.0) * 10 }.each
 
